fbbb2af27a0e585691ac5a0e0308e1881e2a2863
agilsaputra <m.agil.saputra@gmail.com>
2021-08-10 20:18:38 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div>
       <h1>Coba update</h1>
+      <h1>coba update lagi</h1>
       <p>Start editing tao see some magic happen :)</p>
     </div>
   );
