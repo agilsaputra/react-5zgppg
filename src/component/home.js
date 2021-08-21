b2a5a9ba2cf9b9e7b9631680e173824f23b9b753
agilsaputra <m.agil.saputra@gmail.com>
2021-08-21 14:43:42 +0700

    2 files modified, 1 added

--- src/component/home.js
+++ src/component/home.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import "./home.css" ;
 
 const Home = ()=> {
 return (
-      <div>
-          <p>Home Page</p>  
-          
+      <div className="home">
+          <p className="homepage">Home Page</p>  
+          <a href="">link1</a>
+          <a href="">link2</a>
+          <a href="">link3</a>
       </div>
+    
   )
 }
 
