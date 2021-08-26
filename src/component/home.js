ee2f9ad4bd58511816ac81c40e88f72906c91e8e
agilsaputra <m.agil.saputra@gmail.com>
2021-08-26 15:39:04 +0700

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -4,10 +4,8 @@ import { Link } from 'react-router';
 
 const Home = () => {
   return (
-    <div className="home">
-      <p className="homepage">Nike</p>
-    </div>
-  );
+    
+
 };
 
 export default Home;
